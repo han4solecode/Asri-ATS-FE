0114522f97761dcca6b5d39ee8bd6c3619c838b2
han4solecode <farhan@solecode.id>
2024-12-17 10:28:39 +0700

    create auth api call services

---
+++ src/services/auth.service.js
@@ -0,0 +1,33 @@
+import AxiosInstance from "./api";
+
+const registerAplicant = async (userData) => {
+  const res = await AxiosInstance.api.post("/api/auth/applicant", userData);
+  return res;
+};
+
+const login = async (userData) => {
+  const res = await AxiosInstance.api.post("/api/auth/login", userData);
+  localStorage.setItem("user", JSON.stringify(res.data));
+  return res;
+};
+
+const logout = async () => {
+  const res = await AxiosInstance.api.post("/api/auth/logout");
+  localStorage.removeItem("user");
+  return res;
+};
+
+const refreshToken = async () => {
+  const res = await AxiosInstance.api.post("/api/auth/refresh-token");
+  localStorage.setItem("user", JSON.stringify(res.data));
+  return res;
+};
+
+const AuthService = {
+  registerAplicant,
+  login,
+  logout,
+  refreshToken,
+};
+
+export default AuthService;
