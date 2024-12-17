1e50f02296bbdd7c4b0b9853f91b5bd6d67b266f
han4solecode <farhan@solecode.id>
2024-12-17 17:26:21 +0700

    create login/register page layout

---
+++ src/components/Layouts/LoginLayout.jsx
@@ -0,0 +1,36 @@
+import { Outlet, Link } from "react-router-dom";
+
+function LoginLayout(props) {
+  const {} = props;
+
+  return (
+    <div className="flex flex-col min-h-screen justify-between">
+      <nav className="bg-gray-800 p-3">
+        <div className="mx-auto max-w-7xl flex justify-between items-center">
+          <h1 className="text-3xl text-white">
+            {/* <Link to="/">LOGO</Link> */}
+            LOGO
+          </h1>
+        </div>
+      </nav>
+      <div className="container mx-auto max-w-7xl py-3">
+        <div className="flex flex-col">
+          <Outlet></Outlet>
+        </div>
+      </div>
+      <footer className="bg-gray-800 p-3 text-white mt-auto">
+        <div className="mx-auto max-w-7xl flex justify-between sm:flex-col md:flex-row sm:items-center">
+          <div className="flex sm:flex-col md:flex-row sm:items-center md:gap-4 sm:gap-2">
+            {/* change to <Link/> later */}
+            <a href="">Security and Privacy</a>
+            <a href="">Terms and Condition</a>
+            <a href="">Contact</a>
+          </div>
+          <div className="text-gray-400 sm:mt-2">Copyright Text</div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default LoginLayout;
