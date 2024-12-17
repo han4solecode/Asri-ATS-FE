224c6994da1181378fb6a4ab46874acc24e13515
han4solecode <farhan@solecode.id>
2024-12-17 18:37:15 +0700

    fix LoginLayout.jsx layouting and change logo to router link

--- src/components/Layouts/LoginLayout.jsx
+++ src/components/Layouts/LoginLayout.jsx
@@ -8,8 +8,7 @@ function LoginLayout(props) {
       <nav className="bg-gray-800 p-3">
         <div className="mx-auto max-w-7xl flex justify-between items-center">
           <h1 className="text-3xl text-white">
-            {/* <Link to="/">LOGO</Link> */}
-            LOGO
+            <Link to="/">LOGO</Link>
           </h1>
         </div>
       </nav>
@@ -19,14 +18,14 @@ function LoginLayout(props) {
         </div>
       </div>
       <footer className="bg-gray-800 p-3 text-white mt-auto">
-        <div className="mx-auto max-w-7xl flex justify-between sm:flex-col md:flex-row sm:items-center">
-          <div className="flex sm:flex-col md:flex-row sm:items-center md:gap-4 sm:gap-2">
+        <div className="mx-auto max-w-7xl flex justify-between sm:flex-col md:flex-row sm:items-start md:items-center">
+          <div className="flex sm:flex-col md:flex-row sm:items-start md:gap-4 sm:gap-2 underline">
             {/* change to <Link/> later */}
             <a href="">Security and Privacy</a>
             <a href="">Terms and Condition</a>
             <a href="">Contact</a>
           </div>
-          <div className="text-gray-400 sm:mt-2">Copyright Text</div>
+          <div className="text-gray-400 sm:mt-2 md:mt-0">Copyright Text</div>
         </div>
       </footer>
     </div>
