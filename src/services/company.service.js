c2c686aa8fa7c4b57af0ddf6abb64ffa2a227075
han4solecode <farhan@solecode.id>
2024-12-23 16:15:16 +0700

    add method export

--- src/services/company.service.js
+++ src/services/company.service.js
@@ -4,3 +4,9 @@ const registerCompanyRequest = async (userData) => {
   const res = await AxiosInstance.api.post("/api/company/register", userData);
   return res;
 };
+
+const CompanyService = {
+  registerCompanyRequest,
+};
+
+export default CompanyService;
