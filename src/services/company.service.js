0bd47040996080d8f2be74d6ff9e1f6e38ed47ae
han4solecode <farhan@solecode.id>
2024-12-24 10:08:07 +0700

    add review company registration request api service method

--- src/services/company.service.js
+++ src/services/company.service.js
@@ -5,6 +5,14 @@ const registerCompanyRequest = async (userData) => {
   return res;
 };
 
+const reviewRegisterCompanyRequest = async (reviewData) => {
+  const res = await AxiosInstance.api.post(
+    "/api/company/review-request",
+    reviewData
+  );
+  return res;
+};
+
 const getCompany = async (params) => {
   const res = await AxiosInstance.api.get("/api/Company/company", { params });
   return res;
