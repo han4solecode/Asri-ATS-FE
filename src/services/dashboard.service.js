5f97f69027d996304cadfad95b45127adca311c4
han4solecode <farhan@solecode.id>
2025-01-15 09:01:51 +0800

    create admin dashboard api service method

--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -10,9 +10,27 @@ const recruiterDashboard = async (params) => {
   return res;
 };
 
+const adminGetAllCompanyRegistrationRequest = async () => {
+  const res = await AxiosInstance.api.get("/api/dashboard/company-regist-request");
+  return res;
+}
+
+const adminGetAllUsersInfo = async () => {
+  const res = await AxiosInstance.api.get("/api/dashboard/users")
+  return res;
+}
+
+const adminGetAllRoleChangeRequest = async () => {
+  const res = await AxiosInstance.api.get("/api/dashboard/role-change-request");
+  return res;
+}
+
 const DashboardService = {
   applicantDashboard,
   recruiterDashboard,
+  adminGetAllCompanyRegistrationRequest,
+  adminGetAllUsersInfo,
+  adminGetAllRoleChangeRequest,
 };
 
 export default DashboardService;
