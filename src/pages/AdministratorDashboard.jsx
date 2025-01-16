0b90206d2bf2f8d76c2a0d30b68cfba15e3b4a7d
han4solecode <farhan@solecode.id>
2025-01-16 13:44:04 +0800

    add company registration tab at admin dashboard

--- src/pages/AdministratorDashboard.jsx
+++ src/pages/AdministratorDashboard.jsx
@@ -38,6 +38,12 @@ function AdministratorDashboard(props) {
   const [applicantTableRowsPerPage, setApplicantTableRowsPerPage] = useState(5);
   const [internalTablePage, setInternalTablePage] = useState(0);
   const [internalTableRowsPerPage, setInternalTableRowsPerPage] = useState(5);
+  const [companyRegistrationTablePage, setCompanyRegistrationTablePage] =
+    useState(0);
+  const [
+    companyRegistrationTableRowsPerPage,
+    setCompanyRegistrationTableRowsPerPage,
+  ] = useState(5);
 
   useEffect(() => {
     if (selectedTab === 0) {
@@ -101,6 +107,15 @@ function AdministratorDashboard(props) {
     setInternalTablePage(0);
   };
 
+  const handleCompanyRegistrationTableChangePage = (e, newPage) => {
+    setCompanyRegistrationTablePage(newPage);
+  };
+
+  const handleCompanyRegistrationTableChangeRowsPerPage = (e) => {
+    setCompanyRegistrationTableRowsPerPage(+e.target.value);
+    setCompanyRegistrationTablePage(0);
+  };
+
   const handleDeleteUser = (userName) => {
     if (confirm(`Are you sure you want to delete this (${userName}) user? `)) {
       UserService.deleteUser(userName)
@@ -136,7 +151,14 @@ function AdministratorDashboard(props) {
 
       <Box>
         {isLoading ? (
-          <CircularProgress></CircularProgress>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="65vh"
+          >
+            <CircularProgress></CircularProgress>
+          </Box>
         ) : (
           selectedTab === 0 && (
             <div className="flex flex-col gap-4">
@@ -298,10 +320,87 @@ function AdministratorDashboard(props) {
             </div>
           )
         )}
-        {isLoading ? (
-          <CircularProgress></CircularProgress>
-        ) : (
-          selectedTab === 1 && "Company Registration Requests"
+
+        {selectedTab === 1 && (
+          <TableContainer component={Paper} className="shadow-lg">
+            <Table stickyHeader sx={{ maxHeight: 500 }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell className="font-bold">ID</TableCell>
+                  <TableCell className="font-bold">Requester</TableCell>
+                  <TableCell className="font-bold">Email</TableCell>
+                  <TableCell className="font-bold">Company Name</TableCell>
+                  <TableCell className="font-bold">Company Address</TableCell>
+                  <TableCell className="font-bold">Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {companyRegistrations.length > 0 ? (
+                  companyRegistrations
+                    .slice(
+                      companyRegistrationTablePage *
+                        companyRegistrationTableRowsPerPage,
+                      companyRegistrationTablePage *
+                        companyRegistrationTableRowsPerPage +
+                        companyRegistrationTableRowsPerPage
+                    )
+                    .map((cr) => {
+                      return (
+                        <TableRow key={cr.companyRequestId}>
+                          <TableCell>{cr.companyRequestId}</TableCell>
+                          <TableCell>
+                            {cr.firstName} {cr.lastName}
+                          </TableCell>
+                          <TableCell>{cr.email}</TableCell>
+                          <TableCell>{cr.companyName}</TableCell>
+                          <TableCell>{cr.companyAddress}</TableCell>
+                          <TableCell>
+                            <Button
+                              variant="contained"
+                              size="small"
+                              sx={{
+                                backgroundColor: "#1f2937",
+                                color: "white",
+                              }}
+                              onClick={() =>
+                                navigate(
+                                  `/requests/company-registration/review/${cr.companyRequestId}`
+                                )
+                              }
+                            >
+                              View Details
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })
+                ) : (
+                  <TableRow>
+                    <TableCell
+                      colSpan={6}
+                      className="text-center text-gray-600"
+                    >
+                      {error ? error : "No requests found"}
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    count={companyRegistrations.length}
+                    rowsPerPage={companyRegistrationTableRowsPerPage}
+                    page={companyRegistrationTablePage}
+                    onPageChange={handleCompanyRegistrationTableChangePage}
+                    onRowsPerPageChange={
+                      handleCompanyRegistrationTableChangeRowsPerPage
+                    }
+                  ></TablePagination>
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </TableContainer>
         )}
         {selectedTab === 2 && "Role Change Requests"}
       </Box>
